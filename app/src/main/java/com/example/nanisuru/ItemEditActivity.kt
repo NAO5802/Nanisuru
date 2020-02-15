d1bb5f0d4fc0b3e8b74bd9c18f1a9deb6b48ece4
NAO5802 <i100185b@gmail.com>
2020-02-15 22:17:22 +0900

    add item-edit function

--- app/src/main/java/com/example/nanisuru/ItemEditActivity.kt
+++ app/src/main/java/com/example/nanisuru/ItemEditActivity.kt
@@ -17,47 +17,78 @@ class ItemEditActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_edit)
         realm = Realm.getDefaultInstance()
-        // ラジオボタンの初期設定
-        val rg = findViewById<RadioGroup>(R.id.privateSetting)
-        rg.check(R.id.publicBtn)
 
-        // アクティビティ追加
+        val itemId = intent?.getLongExtra("item_id", -1L)
+        if (itemId != -1L) {
+            // DBから値をセット
+            val item = realm.where<Item>().equalTo("id", itemId).findFirst()
+            titleEdit.setText(item?.title)
+            placeEdit.setText(item?.place)
+            memoEdit.setText(item?.memo)
+            val rg = findViewById<RadioGroup>(R.id.privateSetting)
+            when (item?.isPrivate) {
+                false -> rg.check(R.id.publicBtn)
+                true -> rg.check(R.id.privateBtn)
+            }
+        } else {
+            // ラジオボタンの初期設定
+            val rg = findViewById<RadioGroup>(R.id.privateSetting)
+            rg.check(R.id.publicBtn)
+        }
+
+        // 保存ボタン
         saveBtn.setOnClickListener{ view: View ->
             val titleText = titleEdit.text.toString()
             val placeText = placeEdit.text.toString()
             val memoText = memoEdit.text.toString()
 
-            if (titleText.isEmpty()) {
-                titleEdit.error = getString(R.string.requiredError)
-            } else {
-                realm.executeTransaction { db: Realm ->
-                    val maxId = db.where<Item>().max("id")
-                    val nextId = (maxId?.toLong() ?: 0L) + 1
-                    val item = db.createObject<Item>(nextId)
+            when (itemId) {
+                // アクティビティ追加
+                -1L -> {
+                    if (titleText.isEmpty()) {
+                        titleEdit.error = getString(R.string.requiredError)
+                    } else {
+                        realm.executeTransaction { db: Realm ->
+                            val maxId = db.where<Item>().max("id")
+                            val nextId = (maxId?.toLong() ?: 0L) + 1
+                            val item = db.createObject<Item>(nextId)
 
-                    item.title = titleText
-                    item.place = placeText
-                    item.memo = memoText
-                    when (rg.checkedRadioButtonId) {
-                        R.id.publicBtn ->   item.isPrivate = false
-                        R.id.privateBtn ->  item.isPrivate = true
+                            item.title = titleText
+                            item.place = placeText
+                            item.memo = memoText
+                            val rg = findViewById<RadioGroup>(R.id.privateSetting)
+                            when (rg.checkedRadioButtonId) {
+                                R.id.publicBtn ->   item.isPrivate = false
+                                R.id.privateBtn ->  item.isPrivate = true
+                            }
+                        }
+                        Toast.makeText(this, "アクティビティを追加しました", Toast.LENGTH_SHORT)
+                            .show()
                     }
                 }
-                Toast.makeText(this, "アクティビティを追加しました", Toast.LENGTH_SHORT)
-                    .show()
+                // アクティビティ編集
+                else -> {
+                    if (titleText.isEmpty()) {
+                        titleEdit.error = getString(R.string.requiredError)
+                    } else {
+                        realm.executeTransaction{db: Realm ->
+                            val item = db.where<Item>().equalTo("id", itemId).findFirst()
+                            item?.title = titleText
+                            item?.place = placeText
+                            item?.memo = memoText
+                            val rg = findViewById<RadioGroup>(R.id.privateSetting)
+                            when (rg.checkedRadioButtonId) {
+                                R.id.publicBtn ->   item?.isPrivate = false
+                                R.id.privateBtn ->  item?.isPrivate = true
+                            }
+                        }
+                        Toast.makeText(this, "アクティビティを修正しました", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
             }
         }
 
     }
-
-
-//    // エラー文表示
-//    private fun checkError() {
-//        val titleText = titleEdit.text.toString()
-//        if (titleText.isEmpty()) {
-//            titleText.error("文字を入力してください")
-//            return
-//        }
-//    }
 
 }
